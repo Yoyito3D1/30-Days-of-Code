f4f016d5221f23b470cf5b7a31ac0ca91b0dcbf6
Yoyito3D1 <125815824+Yoyito3D1@users.noreply.github.com>
2025-08-31 14:31:06 +0200

    Add files via upload

---
+++ Java 8/Day 25 - Running Time and Complexity.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        int sqrt = (int)Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
+        }
+        sc.close();
+    }
+}
